2f1447f458fdb273288e8c7269a3eaaae3b3b5e4
Udo Rietschel <Udo.Rietschel@bmw.de>
2017-02-06 09:28:58 +0100

    main.cpp formatted using standard clang-format settings

--- learning_modern_cpp/main.cpp
+++ learning_modern_cpp/main.cpp
@@ -14,7 +14,7 @@
 // check: https://www.youtube.com/watch?v=nXaxk27zwlk (minute 42)
 
 //prevents the compiler from optimizing away the variable pointed to by b
-static void escape(void*p) {
+static void escape(void *p) {
     asm volatile("" : : "g"(p) : "memory");
 }
 
@@ -27,25 +27,23 @@ static void clobber() {
 
 // ------------------------------------------------------------------------------------------------------------------
 
-int roll_a_fair_dice()
-{
+int roll_a_fair_dice() {
     static std::default_random_engine e;
-    static std::uniform_int_distribution<int> d{ 1, 6 };
+    static std::uniform_int_distribution<int> d{1, 6};
 
     return d(e);
 }
 
 
-void shuffle_and_display_card_deck()
-{
+void shuffle_and_display_card_deck() {
     using card_t = int;
     using deck_t = std::array<card_t, 52>;
     deck_t deck;
-    std::iota(std::begin(deck), std::end(deck), card_t{ 0 });
+    std::iota(std::begin(deck), std::end(deck), card_t{0});
 
     using engine_t = std::default_random_engine;
     using seed_t   = engine_t::result_type;
-    engine_t e{ seed_t(time(0)) }; //a poor seed;
+    engine_t e{seed_t(time(0))}; //a poor seed;
 
     std::shuffle(std::begin(deck), std::end(deck), e);
 
@@ -63,18 +61,18 @@ void shuffle_and_display_card_deck()
 // SFINAE
 // https://de.wikipedia.org/wiki/Substitution_failure_is_not_an_error
 // https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/enable-if
-template <typename T> // Von T soll bestimmt werden, ob T::Type ein Typ ist
+template<typename T> // Von T soll bestimmt werden, ob T::Type ein Typ ist
 class HasType {
     // Es werden zwei Typen benötigt, die unterschiedlich groß sind, sodass man sie mit sizeof differenzieren kann:
     typedef char FalseType[1];
     typedef char TrueType[2];
 
     // Es folgt die überladene Funktion, mittels welcher SFINAE angewendet wird:
-    template <typename U>
-    static TrueType& Tester(typename U::Type*);
+    template<typename U>
+    static TrueType &Tester(typename U::Type *);
 
-    template <typename>
-    static FalseType& Tester(...);
+    template<typename>
+    static FalseType &Tester(...);
 
 public:
     // Die gesammelte Information wird mit einem einfachen booleschen Wert nach außen gegeben:
@@ -82,8 +80,7 @@ public:
 };
 
 
-void show_off_sfinae()
-{
+void show_off_sfinae() {
     struct Foo {
         typedef int Type;
     };
@@ -100,24 +97,23 @@ void show_off_sfinae()
 
 // ---------------
 // variate_iterator
-template <class URBG, class Dist>
+template<class URBG, class Dist>
 class variate_iterator : public std::iterator<std::input_iterator_tag, typename Dist::result_type> {
 private:
     using iter_t = variate_iterator;
     using val_t  = typename Dist::result_type;
-    using ptr_t  = val_t const*;
-    using ref_t  = val_t const&;
+    using ptr_t  = val_t const *;
+    using ref_t  = val_t const &;
 
-    URBG* u{ nullptr }; //non-owning
-    Dist* d{ nullptr }; //non-owning
+    URBG *u{nullptr}; //non-owning
+    Dist *d{nullptr}; //non-owning
     val_t v{}; // latest variate
-    bool valid{ false };
+    bool valid{false};
 
     //help:
     void step() noexcept { valid = false; }
 
-    ref_t deref()
-    {
+    ref_t deref() {
         if (not valid)
             v = (*d)(*u), valid = true;
         return v;
@@ -126,10 +122,8 @@ private:
 public:
     constexpr variate_iterator() noexcept = default;
 
-    variate_iterator(URBG& u, Dist& d)
-            : u{ &u }
-            , d{ &d }
-    {
+    variate_iterator(URBG &u, Dist &d)
+            : u{&u}, d{&d} {
     }
 
     //dereference
@@ -138,15 +132,13 @@ public:
     ptr_t operator->() { return &deref(); }
 
     //advance
-    iter_t& operator++()
-    {
+    iter_t &operator++() {
         step();
         return *this;
     }
 
-    iter_t operator++(int)
-    {
-        iter_t t{ *this };
+    iter_t operator++(int) {
+        iter_t t{*this};
         step();
         return t;
     }
@@ -154,8 +146,7 @@ public:
 
 // Anwendung der variate_iterators auf Zufallszahlen
 // (liefert bei jedem Aufruf eine neue und kann so zum Füllen eines Vektors mit std::copy_n genutzt werden
-void show_off_variate_iterator()
-{
+void show_off_variate_iterator() {
     using urbg_t    = std::default_random_engine;
     using variate_t = double;
     using dist_t    = std::uniform_real_distribution<variate_t>;
@@ -163,7 +154,7 @@ void show_off_variate_iterator()
     urbg_t g{};
     dist_t d{};
 
-    variate_iterator<urbg_t, dist_t> it{ g, d };
+    variate_iterator<urbg_t, dist_t> it{g, d};
 
     constexpr size_t N = 1000;
     std::vector<variate_t> v(N);
@@ -176,56 +167,53 @@ void show_off_variate_iterator()
 
 
 //template normal programming
-template <class Element>
+template<class Element>
 struct tree_iterator {
     using supports_plus = std::false_type; // member typedef
     // ...
-    tree_iterator& operator++();
+    tree_iterator &operator++();
 };
 
-template <class Element>
+template<class Element>
 struct vector_iterator {
     using supports_plus = std::true_type; // member typedef
     // ...
-    vector_iterator& operator++();
+    vector_iterator &operator++();
 
     vector_iterator operator+(int);
 };
 
-template <class Element>
+template<class Element>
 struct vector {
     using iterator = vector_iterator<Element>; /* ... */
 };
 
-template <class Element>
+template<class Element>
 struct set {
     using iterator = tree_iterator<Element>; /* ... */
 };
 
 
-template <class It>
-It advance_impl(It begin, int n, std::false_type)
-{
+template<class It>
+It advance_impl(It begin, int n, std::false_type) {
     for (int i = 0; i < n; ++i)
         ++begin;
     return begin;
 }
 
-template <class It>
-It advance_impl(It begin, int n, std::true_type)
-{
+template<class It>
+It advance_impl(It begin, int n, std::true_type) {
     return begin + n;
 }
 
-template <class Iter>
-auto advance(Iter begin, int n)
-{
+template<class Iter>
+auto advance(Iter begin, int n) {
     return advance_impl(begin, n, typename Iter::supports_plus{}); // create an object of that type
 }
 
 
-void show_off_template_iterators()
-{
+void show_off_template_iterators() {
+    return;
 }
 
 
@@ -234,11 +222,10 @@ void show_off_template_iterators()
 // template CRTP pattern
 
 //definition of class template
-template <typename CD>
+template<typename CD>
 struct DoubleSpeaker {
-    void speaktwice()
-    {
-        CD* cat_or_dog = static_cast<CD*>(this);
+    void speaktwice() {
+        CD *cat_or_dog = static_cast<CD *>(this);
         cat_or_dog->speak();
         cat_or_dog->speak();
     }
@@ -253,8 +240,7 @@ struct Dog : public DoubleSpeaker<Dog> {
 };
 
 
-void show_off_CRTP_pattern()
-{
+void show_off_CRTP_pattern() {
     Cat c;
     c.speaktwice();
     Dog d;
@@ -275,13 +261,12 @@ struct Number {
     int get() const { return n; }
 };
 
-template <typename BASE, typename T = typename BASE::value_type>
+template<typename BASE, typename T = typename BASE::value_type>
 struct Undoable : public BASE {
     using value_type = T;
     T before;
 
-    void set(T v)
-    {
+    void set(T v) {
         before = BASE::get();
         BASE::set(v);
     }
@@ -289,13 +274,12 @@ struct Undoable : public BASE {
     void undo() { BASE::set(before); }
 };
 
-template <typename BASE, typename T = typename BASE::value_type>
+template<typename BASE, typename T = typename BASE::value_type>
 struct Redoable : public BASE {
     using value_type = T;
     T after;
 
-    void set(T v)
-    {
+    void set(T v) {
         after = v;
         BASE::set(v);
     }
@@ -305,8 +289,7 @@ struct Redoable : public BASE {
 
 using ReUndoableNumber = Redoable<Undoable<Number> >;
 
-void show_off_Mixin_pattern()
-{
+void show_off_Mixin_pattern() {
     ReUndoableNumber mynum;
     mynum.set(42);
     mynum.set(84);
@@ -334,10 +317,10 @@ void show_off_Mixin_pattern()
 //};
 
 // modern C++ allows for a shorter form
-template <class T>
+template<class T>
 struct is_void : std::false_type {
 };
-template <>
+template<>
 struct is_void<void> : std::true_type {
 };
 
@@ -353,11 +336,11 @@ struct is_void<void> : std::true_type {
 
 
 // again, shorter form due to modern C++
-template <class T>
+template<class T>
 struct is_pointer : std::false_type {
 };
-template <class T>
-struct is_pointer<T*> : std::true_type {
+template<class T>
+struct is_pointer<T *> : std::true_type {
 };
 
 
@@ -370,36 +353,33 @@ class ObjectA {
 class ObjectB {
 public:
     //not necessary as we see below, but this object itslef could have the specialized algorithm as public member functions
-    void optimised_implementation()
-    {
+    void optimised_implementation() {
         std::cout << "Optimized implementation of algorithm for Objects of type 'ObjectB' as a method of ObjectB\n";
     }
 };
 
 // again, first the default traits class is created (exactly analogous to is_void above)
-template <typename T>
+template<typename T>
 struct supports_optimised_implementation : std::false_type {
 };
 // and a specialisation of supports_optimised_implementation trait for ObjectB
-template <>
+template<>
 struct supports_optimised_implementation<ObjectB> : std::true_type {
 };
 
 
-template <bool b> // does it matter if this param ist bool or int or ...?
+template<bool b> // does it matter if this param ist bool or int or ...?
 struct algorithm_selector {
-    template <typename T>
-    static void implementation(T& object)
-    {
+    template<typename T>
+    static void implementation(T &object) {
         std::cout << "Standard implementation of algorithm\n";
     }
 };
 
-template <>
+template<>
 struct algorithm_selector<true> {
-    template <typename T>
-    static void implementation(T& object)
-    {
+    template<typename T>
+    static void implementation(T &object) {
         std::cout << "Optimized implementation of algorithm for Objects of type 'ObjectB' exists\n";
 
         // specialized Implementation could be implemented HERE directly or we can have the object itself take care of that like in this example
@@ -413,19 +393,17 @@ void show_off_threading_with_futures();
 
 // a generic function that the end user of the algorithm will call
 // note that it in turn calls algorithm_selector, parameterised using the supports_optimised_implementation traits class:
-template <typename T>
-void algorithm(T& object)
-{
+template<typename T>
+void algorithm(T &object) {
     algorithm_selector<supports_optimised_implementation<T>::value>::implementation(object);
 }
 
 
-void show_off_traits()
-{
-    bool is_void_1   = is_void<void>::value;
-    bool is_void_2   = is_void<void*>::value;
+void show_off_traits() {
+    bool is_void_1 = is_void<void>::value;
+    bool is_void_2 = is_void<void *>::value;
     bool is_pointer1 = is_pointer<void>::value;
-    bool is_pointer2 = is_pointer<void*>::value;
+    bool is_pointer2 = is_pointer<void *>::value;
 
 
     ObjectA a;
@@ -443,8 +421,8 @@ struct Exchange {
     double rates[2];
 
     Exchange(const std::initializer_list<double> il) {
-        if( il.size() < 2 ) return;
-        if( il.size() == 3 ) {
+        if (il.size() < 2) return;
+        if (il.size() == 3) {
             auto it = il.begin();
             count = static_cast<int>(*it++);
             rates[0] = *it++;
@@ -457,27 +435,25 @@ struct Exchange {
     }
 };
 
-void show_off_uniform_initialization()
-{
+void show_off_uniform_initialization() {
 
-    std::vector<std::string> strvec1 = { "string1", "string2" };
+    std::vector<std::string> strvec1 = {"string1", "string2"};
 
     // this does NOT give the same result-> strvec2 will be deduced to be of type std::initializer_list<char const*>
-    auto strvec2 = { "string1", "string2" };
+    auto strvec2 = {"string1", "string2"};
 
-    std::map<int, std::string> map1 = { { 1, "One" }, { 2, "Two" } };
+    std::map<int, std::string> map1 = {{1, "One"},
+                                       {2, "Two"}};
 
 
-    Exchange exchange{42,1.2,2.3};
+    Exchange exchange{42, 1.2, 2.3};
 
-    return;
 }
 
 // ------------------------------------------------------------------------------------------------------------------
 
 
-void show_off_move_and_forward()
-{
+void show_off_move_and_forward() {
 
     return;
 }
@@ -486,15 +462,13 @@ void show_off_move_and_forward()
 // ------------------------------------------------------------------------------------------------------------------
 
 
-template <typename T>
-void VariadicTemplateFunction(T t)
-{
+template<typename T>
+void VariadicTemplateFunction(T t) {
     std::cout << t << '\n';
 }
 
-template <typename T, typename... Data>
-void VariadicTemplateFunction(T t, Data... d)
-{
+template<typename T, typename... Data>
+void VariadicTemplateFunction(T t, Data... d) {
 
     std::cout << "# of params remaining: " << sizeof...(d) << " - current value: " << t << '\n';
     VariadicTemplateFunction(d...);
@@ -502,20 +476,18 @@ void VariadicTemplateFunction(T t, Data... d)
 
 
 // second way: check https://www.youtube.com/watch?v=CU3VYN6xGzM&index=10&list=PLs3KjaCtOwSZ2tbuV1hx8Xz-rFZTan2J1
-template <typename T>
-void my_println_impl(const T& param)
-{
+template<typename T>
+void my_println_impl(const T &param) {
     std::cout << param << '\n';
 }
 
-template <typename ... T>
-void VariadicTemplateFunctionWithInitializerListTrick(const T& ... param) {
+template<typename ... T>
+void VariadicTemplateFunctionWithInitializerListTrick(const T &... param) {
 
-    (void)std::initializer_list<int>{ (my_println_impl(param), 0)... };
+    (void) std::initializer_list<int>{(my_println_impl(param), 0)...};
 }
 
-void show_off_variadic_templates()
-{
+void show_off_variadic_templates() {
     VariadicTemplateFunction(1, 2, 3, 4, 5, 6, 7, 8);
 
     VariadicTemplateFunctionWithInitializerListTrick("Hello", "World", 1, 2, 3, 4.2);
@@ -545,9 +517,7 @@ void show_off_threading_with_futures() {
 
 
 // ------------------------------------------------------------------------------------------------------------------
-
-int main()
-{
+int main() {
 
     //Beispiel (noch refaktorieren)
     //Ein Array mit Würfelergebnissen eines 6-seitigen Würfels füllen
